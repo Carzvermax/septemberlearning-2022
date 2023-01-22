785b1e44a646a06d395cd4f2162c6ff7abb0efac
Carzvermax <carzvermax@gmail.com>
2023-01-22 20:07:51 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Simpsons from "./components/Simpsons/Simpsons";
+import Characters from "./components/Characters/Characters";
+
+const App = () => {
+  return (
+    <div className="App">
+    {/*<Simpsons/>*/}
+    <Characters/>
+    </div>
+  );
+}
+
+export default App;
