daf68000f8ffa2edc0c1221a8969913ee9607461
Carzvermax <carzvermax@gmail.com>
2023-01-25 21:01:50 -0400

    masterUpdate2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import Simpsons from "./components/Simpsons/Simpsons";
-import Characters from "./components/Characters/Characters";
+
 
 const App = () => {
   return (
