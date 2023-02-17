0455bfa1c8cc51a672192c66fe2f08da85f5393a
Carzvermax <carzvermax@gmail.com>
2023-02-17 18:11:40 -0400

    HW_7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import {Cars, Form} from "./components";
+import {useSelector} from "react-redux";
+import {Navigate, Route, Routes} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import AuthRequireLayout from "./layouts/AuthRequireLayout";
+import CarsPage from "./pages/CarsPage/CarsPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
-  );
-}
+      <Routes>
+          <Route path={'/'} element={<MainLayout/>}>
+              <Route index element={<Navigate to={'cars'}/>}/>
 
-export default App;
+              <Route element={<AuthRequireLayout/>}>
+                  <Route path={'cars'} element={<CarsPage/>}/>
+              </Route>
+
+              <Route path={'login'} element={<LoginPage/>}/>
+              <Route path={'register'} element={<RegisterPage/>}/>
+          </Route>
+      </Routes>
+  );
+};
+
+export {App};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
+
 import './index.css';
-import App from './App';
+import {App} from './App';
+import {setupStore} from "./redux";
+import {unstable_HistoryRouter as BrowserRouter} from "react-router-dom";
+import {history} from "./services";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = setupStore();
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <BrowserRouter history = {history}>
+        <App/>
+        </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
